0af4b56ab6e571201676d3b54b9a37d6a59d7219
levi-sr93 <levi-sr@outlook.com>
2020-12-11 10:15:13 -0300

    Suggestions component| ScrollView Horizontal| styled components| attrs

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 import React from "react";
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StatusBar } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import Navigation from "./Navigation";
 
 export default function App() {
   return (
     <NavigationContainer>
+      <StatusBar barStyle="light-content" backgroundColor="#000"/>
       <Navigation />
     </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({});
